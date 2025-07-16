3ebadb50d16032b38777e9d9643b4006c7550812
Shweta9031 <shwetassrr@gmail.com>
2025-07-16 23:00:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+//toggle icon-bar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//const web = document.getElementById('web');
+//let origin = ["Frontend devloper","Backend Devloper"];
+
+//let i = 0
+//function updateWeb (){
+  //  web.textContent = origin[i];
+    //i++;
+    //if (i == 2) {
+      //  i = 0;
+   // }
+//}
+
+//setInterval(updateWeb,500);
+
+//scroll section
+let sections = document.querySelectorAll('section') ;
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top <offset+height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            //active sections for Animation on scroll
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+
+    })
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100 );
+
+   // remove toggle icon and navbar when click navbar links (scrolls)
+
+   menuIcon.classList.remove('bx-x');
+   navbar.classList.remove('active');
+
+   /*animation footer on scroll*/
+   
+   let footer = document.querySelector('footer');
+
+   footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= 
+   document.scrollingElement.scrollHeight);
+
+}
